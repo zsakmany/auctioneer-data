7540818ae39e49ad1c6ec532c4d29686f5b7a408
Viktor Horvath <zsakmany@gmail.com>
2017-11-23 09:00:14 +0100

    Smoke testing if it works as expected. Every Auctioneer related file added.

---
+++ Auc-Advanced.lua
@@ -0,0 +1,59 @@
+
+AucAdvancedConfig = {
+	["users.Bloodscalp.Throonrak"] = "Default",
+	["profile.Default"] = {
+		["stat"] = {
+			["sales"] = {
+				["upgraded32"] = true,
+				["ignoredsigs"] = {
+				},
+			},
+		},
+		["util"] = {
+			["automagic"] = {
+				["ammailguix"] = 160,
+				["uierrormsg"] = 1,
+				["SavedMailButtons"] = {
+				},
+			},
+		},
+	},
+	["version"] = 1,
+}
+AucAdvancedData = {
+	["UtilSearchUiData"] = {
+		["Global"] = {
+			["configator.left"] = 510.000030517578,
+			["configator.top"] = 790,
+		},
+		["Current"] = {
+			["snatch.itemsList"] = {
+			},
+		},
+		["Version"] = 1,
+		["SavedSearches"] = {
+		},
+	},
+	["Stats"] = {
+	},
+}
+AucAdvancedServers = {
+	["ConnectedRealmTables"] = {
+		["#Bloodscalp"] = {
+			"Agamaggan", -- [1]
+			"Bloodscalp", -- [2]
+			"Crushridge", -- [3]
+			"Emeriss", -- [4]
+			"Hakkar", -- [5]
+			"Twilight'sHammer", -- [6]
+		},
+	},
+	["Version"] = 1,
+	["KnownRealms"] = {
+		["Bloodscalp"] = "#Bloodscalp",
+	},
+	["Timestamp"] = 1511387473,
+	["KnownServerKeys"] = {
+		["#Bloodscalp"] = 1511416592,
+	},
+}
---
+++ Auc-Filter-Basic.lua.bak
@@ -0,0 +1,7 @@
+
+AucAdvancedFilterBasic_IgnoreList = {
+	["Bloodscalp"] = {
+		["List"] = {
+		},
+	},
+}
