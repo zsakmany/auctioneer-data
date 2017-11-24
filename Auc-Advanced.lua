8fd492f24eba2127f7480e66f4abb0b86c48faff
Horvath Viktor <zsakmany@gmail.com>
2017-11-24 13:02:21 +0100

    Lunch break! Here are the newest results.

--- Auc-Advanced.lua
+++ Auc-Advanced.lua
@@ -12,50 +12,56 @@ AucAdvancedConfig = {
 		},
 		["util"] = {
 			["appraiser"] = {
+				["item.47643.match"] = true,
 				["item.127836.numberonly"] = false,
+				["item.127836.fixed.bid"] = 39867,
+				["item.127836.match"] = false,
+				["item.127836.model"] = "fixed",
+				["item.37921.model"] = "Histogram",
+				["item.127836.stack"] = 20,
+				["item.47643.model"] = "StdDev",
+				["item.127836.fixed.buy"] = 40963,
+				["item.47643.duration"] = 1440,
+				["columnsortcurDir"] = 1,
 				["columnsortcurSort"] = 6,
 				["item.37704.model"] = "Histogram",
-				["item.127836.model"] = "market",
-				["columnsortcurDir"] = -1,
-				["item.127836.stack"] = 10,
-				["item.127836.number"] = 5,
-				["item.37921.model"] = "Histogram",
+				["item.127836.number"] = -1,
 			},
 			["automagic"] = {
 				["ammailguix"] = 160,
+				["uierrormsg"] = 1,
 				["SavedMailButtons"] = {
 				},
-				["uierrormsg"] = 1,
 			},
 			["ahwindowcontrol"] = {
-				["ahframeanchors"] = "TOPLEFT:TOPLEFT:-0.0:-89.6",
+				["ahframeanchors"] = "TOPLEFT:TOPLEFT:0.0:-89.6",
 			},
 		},
 		["stat"] = {
 			["sales"] = {
+				["upgraded32"] = true,
 				["ignoredsigs"] = {
 				},
-				["upgraded32"] = true,
 			},
 		},
 	},
 	["version"] = 1,
 }
 AucAdvancedData = {
-	["Stats"] = {
-	},
 	["UtilSearchUiData"] = {
-		["Version"] = 1,
-		["Current"] = {
-			["snatch.itemsList"] = {
-			},
-		},
 		["Global"] = {
 			["configator.left"] = 510.000030517578,
 			["configator.top"] = 790,
 		},
+		["Current"] = {
+			["snatch.itemsList"] = {
+			},
+		},
+		["Version"] = 1,
 		["SavedSearches"] = {
 		},
+	},
+	["Stats"] = {
 	},
 }
 AucAdvancedServers = {
@@ -73,8 +79,8 @@ AucAdvancedServers = {
 	["KnownRealms"] = {
 		["Bloodscalp"] = "#Bloodscalp",
 	},
-	["KnownServerKeys"] = {
-		["#Bloodscalp"] = 1511512295,
-	},
 	["Timestamp"] = 1511387473,
+	["KnownServerKeys"] = {
+		["#Bloodscalp"] = 1511523441,
+	},
 }
